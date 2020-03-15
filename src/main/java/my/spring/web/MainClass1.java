3ba93cd66807dcd9e119a649aa88cb3e3dd5f24c
admin <admin@example.com>
2020-03-15 17:57:39 +0800

    four commit

--- src/main/java/my/spring/web/MainClass1.java
+++ src/main/java/my/spring/web/MainClass1.java
@@ -2,6 +2,6 @@ package my.spring.web;
 
 public class MainClass1 {
   public static void main(String[] args) {
-    System.out.println("hello world 1");
+    System.out.println("hello world 1 dev");
   }
 }
--- src/main/java/my/spring/web/MainClass2.java
+++ src/main/java/my/spring/web/MainClass2.java
@@ -2,6 +2,6 @@ package my.spring.web;
 
 public class MainClass2 {
   public static void main(String[] args) {
-    System.out.println("hello world 2");
+    System.out.println("hello world 2 dev");
   }
 }
